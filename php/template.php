c707432fd5e878de981357706fff3fb4aa027690
root <user.loca@gmail.com>
2015-11-30 12:14:17 +1100

    [add] php/template.php

---
+++ php/template.php
@@ -0,0 +1,35 @@
+<?php namespace miv; ?>
+<?php
+/*
+ *  php - class - template
+ *
+ */
+class template {
+
+
+
+
+    /** | magic **/
+    /** magic | **/
+
+
+    /** | get **/
+    /** get | **/
+
+
+    /** | set **/
+    /** set | **/
+
+
+    /** | valid **/
+    /** valid | **/
+
+
+
+
+}
+/*
+ *  php - class - template
+ *
+ */
+?>
