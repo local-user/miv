1ab2967f73a75e6a8ae02469b5b99f4f09c75c7d
root <user.loca@gmail.com>
2015-12-12 16:39:44 +1100

    [update] overlay hide on keypress

--- webroot/js/jquery/listener/element/welcome.js
+++ webroot/js/jquery/listener/element/welcome.js
@@ -9,22 +9,24 @@ console.log(' . js/jquery/listener/element/welcome.js');
 
 
 
-    // mouseover - welcome
+
+    // click - welcome - display - hide
+    $( '#welcome' ).click(function() {
+
+        // overlay - display - hide
+        overlay.display_hide();
+
+        // return
+        return true;
+
+    });
+
+
+    // mouseover - key - enter - welcome - disply - hide
     $( '#welcome' ).mouseover( function() {
 
-        // click - welcome - display - hide
-        $( '#welcome' ).click(function() {
-
-            // overlay - display - hide
-            overlay.display_hide();
-
-            // return
-            return true;
-
-        });
-
-        // key - welcome - display - hide
-        $(document).keyup(function (e){
+        // key - press
+        $( document ).keydown(function (e){
 
             // overlay - display - hide
             overlay.display_hide();
